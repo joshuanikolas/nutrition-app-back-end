775a85f09c3731ad2b2aab241da38ba4310ad2e6
joshuanikolas <nikolassnchez@icloud.com>
2024-12-10 15:16:08 -0600

    npm i cors added

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 const usersRouter = require('./controllers/users');
 const foodsRouter = require('./controllers/foods');
 const testJWTRouter = require('./middleware/verify-token')
-
-
+const cors = require('cors');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -15,16 +14,11 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-
-// Routes
 app.use(express.json());
 app.use('/users', usersRouter);
 app.use('/foods', foodsRouter);
 app.use('/jwt-test', testJWTRouter)
-
-
-
-
+app.use(cors())
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
